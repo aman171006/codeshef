f2716d27b51bc02e5e932efd53b3ef05f665c146
Aman Singh <55836645+aman171006@users.noreply.github.com>
2020-07-28 05:20:28 -0700

    First Problem

--- Practice(beginner)/ATM(HS08TEST).py
+++ Practice(beginner)/ATM(HS08TEST).py
@@ -3,7 +3,8 @@
 #                               |          |       You can use map() on any iterable variables not only input
 #                               |          |
 with_draw,total_balance = map(float,input().split())
-if (with_draw%5) ==0 and (with_draw+0.50) <= total_balance :        # 0.50 is added to the withdrawal money as interest is paid by your account only
+
+if ( with_draw % 5 ) ==0 and ( with_draw + 0.50 ) <= total_balance :        # 0.50 is added to the withdrawal money as interest is paid by your account only
     print("{0:0.2f}".format(total_balance-with_draw-0.50))          # print("any_string1 {} any_string2 {}".format(var1,var2)) easier to read the code and {0:0.2f} for accuracy.
 else :
     print("{0:0.2f}".format(total_balance))
